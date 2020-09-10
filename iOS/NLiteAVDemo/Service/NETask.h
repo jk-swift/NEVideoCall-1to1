a0453b26c2e9dbf304d5fd180447796f5eb97cb8
gyy <7645421+gyy123456@user.noreply.gitee.com>
2020-09-10 13:48:15 +0800

    https

--- iOS/NLiteAVDemo/Service/NETask.h
+++ iOS/NLiteAVDemo/Service/NETask.h
@@ -13,7 +13,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 // 请求闭包
 typedef void(^NERequestHandler)(NSDictionary * _Nullable data, NSError * _Nullable error);
-static NSString *baseURL = @"http://yiyong.netease.im";
+static NSString *baseURL = @"https://yiyong.netease.im";
 
 @interface NETask : NSObject<NEServiceTask>
 + (instancetype)task;
